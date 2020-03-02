7f507be3b52d8114f4292b3c57e0471bae2a0ad7
Aman jha <aman.kumar.jha953@gmail.com>
2020-03-03 01:49:19 +0530

    Read value from text area fields added module

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,54 @@
-const mainController = (function(){
+// Notepad Controller
+const appController = (function(){
+
     return {
         DOMElements: {
             settings : document.querySelector('#settings'),
             notifications: document.querySelector('#notifications'),
-            userPanel: document.querySelector('#userpanel')
+            userPanel: document.querySelector('#userpanel'),
+            textInput: document.querySelector('.text-input'),
+            addbtn: document.querySelector('.addbtn'),
         }
     }
 })();
 
-
+// Edittor App UI controller ,  It will update UserInterface
 const UIController = (function(){
 
+    // Public method 
+
+    return {
+        inputdata : () => {
+            return inputValue = appController.DOMElements.textInput.value;
+        }
+    }
 })();
 
-
-const AppController = (function(mainCtrl, UIctrl){
-    mainController.DOMElements.settings.addEventListener('click',hello);
-
-    function hello(){
-        console.log('hey.....')
+// Main Controller which handle operation of both modules
+const controller = (function(appCtrl, UIctrl){
+    
+    const ctrlAddItem = () => {
+        // 1. Get the field input data
+       const input =  UIctrl.inputdata();
+        // 2. Add the Items to the Controller 
+    
+        // 3. Add the New Items to UIController module
+        console.log(input);
     }
-     return {
 
+    appCtrl.DOMElements.addbtn.addEventListener('click', ctrlAddItem)
+    appCtrl.DOMElements.textInput.addEventListener('keypress', function(event){
+        
+        if(event.keycode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+    
+    function hello(){
+        
+    }
+
+    return {
      }
 
-})(mainController, UIController);
+})(appController, UIController);
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 // Global Notepad app Controller
-import {addnum} from '../test';
 import '../scss/main.scss' 
 import 'bootstrap'; 
-console.log(`module imported from another planet ${addnum}`);
+import './app';
