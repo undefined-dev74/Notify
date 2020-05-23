594d83fba0eca00c337dbe02e7d95f7c0ea92cce
JuniorBaymx <cooladitya953@gmail.com>
2020-05-23 21:38:09 +0530

    connected with child component ViewList.jsx

--- src/components/ViewGroup.jsx
+++ src/components/ViewGroup.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
+import ViewList from './ViewList';
 
 class ViewGroup extends React.Component {
-	constructor(props) {
-		super(props);
-		const { text, key } = props;
-	}
 	render() {
+		const { items } = this.props;
 		return (
 			<section className="container">
-				<div className="view shadow-lg">
-						<div className="view-items">
-							<h3 className="view-items-text">Wash car Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, ut!</h3>
-						</div>
-						<div className="view-timeline ">
-							<p className="view-today mr-2">Today</p>
-							<p className="view-time">6:00am</p>
-						</div>
-						<div className="view_icons">
-							<span className="mx-2 text-success">
-								<i className="fas fa-pen"></i>
-							</span>
-							<span className="mx-2 text-danger">
-								<i className="fas fa-trash"></i>
-							</span>
-						</div>
-				</div>
+				{items.map((item) => {
+					return <ViewList key={item.id} title={item.title} />;
+				})}
 			</section>
 		);
 	}
