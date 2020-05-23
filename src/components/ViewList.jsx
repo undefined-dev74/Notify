f3e4b67d8242c6e7e4c897570a4c9e5e0eafc179
JuniorBaymx <cooladitya953@gmail.com>
2020-05-23 21:37:26 +0530

    added invidual todo list components

---
+++ src/components/ViewList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// import Viewgroup from './ViewGroup';
+
+export default class ViewList extends React.Component {
+	render() {
+		const { title } = this.props;
+		return (
+			<div className="view shadow-lg">
+				<div className="view-items">
+					<h3 className="view-items-text">{title}</h3>
+				</div>
+				<div className="view-timeline ">
+					<p className="view-today mr-2">Today</p>
+					<p className="view-time">6:00am</p>
+				</div>
+				<div className="view_icons">
+					<span className="mx-2 text-success">
+						<i className="fas fa-pen"></i>
+					</span>
+					<span className="mx-2 text-danger">
+						<i className="fas fa-trash"></i>
+					</span>
+				</div>
+			</div>
+		);
+	}
+}
