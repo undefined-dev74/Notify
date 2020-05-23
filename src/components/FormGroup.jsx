64b336cb380bb0c183bdd4ece6cea382954ccecc
JuniorBaymx <cooladitya953@gmail.com>
2020-05-23 20:55:57 +0530

    updated props from app.js

--- src/components/FormGroup.jsx
+++ src/components/FormGroup.jsx
@@ -3,24 +3,18 @@ import React from 'react';
 class FormGroup extends React.Component {
 	
 
-	addItem = (event) => {
-		event.preventDefault();
-		console.log('click fired')
-	};
 
 	render() {
-		const {item, handleInputChange} = this.props;
+		const {item, handleInputChange, handleSubmit} = this.props;
 		return (
-			<div className="form_group">
-				<textarea
+			<form className="form_group" onSubmit={handleSubmit}>
+				<input 
 					className=" text-input form-control shadow-sm"
-					rows="2"
-					aria-label="With textarea"
 					placeholder="write a note or paste a link"
 					value={item }
-					onChange={handleInputChange}></textarea>
-				<i className="form_group-icon addbtn fas fa-2x fa-plus-circle" onClick={this.addItem}></i>
-			</div>
+					onChange={handleInputChange}></input>
+				<i className="form_group-icon addbtn fas fa-2x fa-plus-circle" onClick={handleSubmit}></i>
+			</form>
 		);
 	}
 }
