11e2a5967607f5489a1678471873f0307c6e6022
JuniorBaymx <cooladitya953@gmail.com>
2020-05-24 18:06:38 +0530

    added Sweetalert custom Alert func

--- src/components/FormGroup.jsx
+++ src/components/FormGroup.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import swal from 'sweetalert'
 
 class FormGroup extends React.Component {
 	render() {
 		const { item, handleInputChange, handleSubmit, editItem } = this.props;
 		return (
-			<form className="form_group" onSubmit={handleSubmit}>
+			<form
+				className="form_group"
+				onSubmit={(e) => { e.preventDefault()
+					item !== '' ? handleSubmit() :swal('Text Missing','You must continue with some text',"warning" );
+				}}>
 				<input
 					className=" text-input form-control shadow-sm"
 					placeholder="write a note or paste a link"
 					value={item}
 					onChange={handleInputChange}
-					type='text'
-					maxLength={80}
-					></input>
+					type="text"
+					maxLength={80}></input>
 				<i
 					className="form_group-icon addbtn fas fa-2x fa-plus-circle"
-					onClick={handleSubmit}></i>
+					onClick={() => {
+						item !== ''
+							? handleSubmit()
+							: swal('Text Missing','You must continue with some text',"warning" );
+					}}></i>
 				<div className="clearlist text-center">
-					<button className={editItem ? 'btn btn-danger btn-success' : 'btn btn-danger'}>
+					<button
+						className={
+							editItem ? 'btn btn-danger btn-success' : 'btn btn-danger'
+						}>
 						{editItem ? 'Edit note' : 'save note'}
 					</button>
 				</div>
