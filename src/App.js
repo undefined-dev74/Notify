533d0523b36af17525a787517d7e75fda69b89ea
JuniorBaymx <cooladitya953@gmail.com>
2020-05-30 23:01:31 +0530

    source updated to master now work tree will be Master branch

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class App extends React.Component {
 	handleEdit = (id) => {
 		console.log(id);
 		const filteredItems = this.state.items.filter((item) => item.id !== id);
-
+		const arr = []
 		const selectedItem = this.state.items.find((item) => item.id === id);
 		console.log(selectedItem);
 		this.setState({
