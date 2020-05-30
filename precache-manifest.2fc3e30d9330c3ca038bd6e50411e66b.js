bcf9e0ffaadbb770297b5bd6e3c1a3d26bdf99da
JuniorBaymx <cooladitya953@gmail.com>
2020-05-30 23:41:52 +0530

    Updates

---
+++ precache-manifest.2fc3e30d9330c3ca038bd6e50411e66b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99be7ea074622bb3df3f58806f614736",
+    "url": "/EditNoter/index.html"
+  },
+  {
+    "revision": "086185c15f9771e88df3",
+    "url": "/EditNoter/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8655f5dd577210d89164",
+    "url": "/EditNoter/static/css/main.878caa0a.chunk.css"
+  },
+  {
+    "revision": "086185c15f9771e88df3",
+    "url": "/EditNoter/static/js/2.1fa5031a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/EditNoter/static/js/2.1fa5031a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8655f5dd577210d89164",
+    "url": "/EditNoter/static/js/main.d90f0cf5.chunk.js"
+  },
+  {
+    "revision": "bf1b66dca5f2571b7b73",
+    "url": "/EditNoter/static/js/runtime-main.8301938a.js"
+  }
+]);
